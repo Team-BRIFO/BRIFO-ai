0ab7098d9c38d60428bb2c42e3f7619f458bc5aa
yunnij <blrdyunnij@gmail.com>
2026-07-13 15:37:46 +0900

    feat: main에 라우터 추가

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
 
+from app.api import briefing, health, news
+
 app = FastAPI()
+
+app.include_router(health.router)
+app.include_router(briefing.router)
+app.include_router(news.router)
